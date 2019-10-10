6b6959277765797175524272fc566e2529cee398
lperezp <luiseperezp.10@gmail.com>
2019-10-10 17:06:27 -0500

    feat (React): A method was added.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './assets/img/logo.svg';
 import './assets/css/App.css';
 
-// 
+// import component
+import MyComponent from './components/MyComponent';
+
+
+function HolaMundo(nombre){
+  var presentacion = <h3>I'm { nombre }</h3>;
+  return presentacion;
+}
 
 function App() {
+  var nombre = `Luis Perez`;
+  
   return (
+    /* { alert("Hola Mundo") } */
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>This is my first web application in React.</p>
+        { HolaMundo(nombre) }
+        <p>Visit my website <a className="App-link" href="https://lperezp.com" target="_blank" rel="noopener noreferrer">lperezp</a></p>
       </header>
     </div>
   );
---
+++ src/components/MyComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class MyComponent extends React.Component{
+
+    render(){
+        return (<h1>Hi! I'am MyComponent</h1>)
+    }
+}
+
+export default MyComponent;
