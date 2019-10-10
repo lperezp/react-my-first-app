54930d3c04ac807d4f4eaff82c9f3facf6d4704c
lperezp <luiseperezp.10@gmail.com>
2019-10-10 16:20:32 -0500

    fix (React): Creating a base structure.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/img/logo.svg';
+import './assets/css/App.css';
+
+// 
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
